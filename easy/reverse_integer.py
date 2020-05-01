220e33a0f91ee75c8a26883a52a1bf1a1f1de066
Debeljak Savo <savo.debeljak96@gmail.com>
2020-05-01 22:49:31 +0200

    Update .gitignore file. Solve first two easy problems: Two Sum and Reverse Integer.

---
+++ easy/reverse_integer.py
@@ -0,0 +1,66 @@
+# Given a 32-bit signed integer, reverse digits of an integer.
+
+# Example 1:
+
+# Input: 123
+# Output: 321
+
+# Example 2:
+
+# Input: -123
+# Output: -321
+
+# Example 3:
+
+# Input: 120
+# Output: 21
+
+# Note:
+# Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        # cheating
+        # string = str(x)
+        # res = 0
+        # if x >= 0:
+        #     res = int(string[::-1])
+        # else: 
+        #     string = string[1::]
+        #     res = -int(string[::-1])
+        
+        # if res not in range(-2**31, 2**31 - 1): 
+        #     return 0
+        
+        # return res
+
+        res = 0
+        # because python is stupid and % doesn't work as intended
+        neg = x < 0
+        x = abs(x)
+        while x != 0:
+            res = res * 10 + x % 10
+            x //= 10
+        
+        if res not in range(-2**31, 2**31 - 1): 
+            return 0
+        
+        return res if not neg else -res
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.reverse(123))
+    print(sol.reverse(-123))
+    print(sol.reverse(120))
+    print(sol.reverse(1534236469))
+
+# Cheating: 
+# Runtime: 32 ms, faster than 57.14% of Python3 online submissions for Reverse Integer.
+# Memory Usage: 13.6 MB, less than 5.26% of Python3 online submissions for Reverse Integer.
+
+# Runtime: 24 ms, faster than 95.15% of Python3 online submissions for Reverse Integer.
+# Memory Usage: 13.9 MB, less than 5.26% of Python3 online submissions for Reverse Integer.
---
+++ easy/two_sum.py
@@ -0,0 +1,33 @@
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# Example:
+
+# Given nums = [2, 7, 11, 15], target = 9,
+
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        lookup_table = {}
+        for index, num in enumerate(nums):
+            complement = target - num
+            if complement in lookup_table: 
+                return [lookup_table[complement], index]
+            else: 
+                lookup_table[num] = index
+        return None
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.twoSum([2, 7, 11, 15], 9))
+
+# Runtime: 44 ms, faster than 91.99% of Python3 online submissions for Two Sum.
+# Memory Usage: 15.1 MB, less than 5.11% of Python3 online submissions for Two Sum.
